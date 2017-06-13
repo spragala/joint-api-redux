e8da9abdf6672daef7ca166e5c128fcbd133c806
spragala <spragala@yahoo.com>
2017-06-13 16:01:13 -0700

    data being passed to Card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
-import Card from './components/Card'
+import Tarpy from './container/Tarpy'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Card />
+        <Tarpy />
       </div>
     );
   }
