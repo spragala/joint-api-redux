16c0a2fd8464489075d243bb75d90794d8d05329
spragala <spragala@yahoo.com>
2017-06-13 15:13:43 -0700

    installed jquery-ajax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
+import Card from './components/Card'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Card />
       </div>
     );
   }
